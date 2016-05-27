770c1ecaafbb94a9860c5e9b392a357c986c1dd1
MarcStan <marcstan@live.com>
2016-05-27 22:08:13 +0200

    updated nuget packages & license

--- src/AssemblyInfo/AssemblyInfoInc.cs
+++ src/AssemblyInfo/AssemblyInfoInc.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 - 2016")]
